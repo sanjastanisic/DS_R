1320eae3c30d58fecc02ac5cc1eb108016d2d881
sanjastanisic <sanjastanisic@t-com.me>
2019-08-03 17:08:31 +0200

    dodati komentari es01

--- 01_Esercizio.R
+++ 01_Esercizio.R
@@ -1,29 +1,29 @@
 # 1.a 
 
-getwd() 
+getwd() # vraca Rdni direktorijum
 
 # 1.b
 
-setwd("C:/Users/Sanja/Documents/_Sanja/Bicocca - studio/Data Science/IntroductionToR/DS_R")
+setwd("C:/Users/Sanja/Documents/_Sanja/Bicocca - studio/Data Science/IntroductionToR/DS_R") # postavlja radni direktorijum
 
 # 2.a 
 
-rm (list = ls())
+rm (list = ls()) # uklanja sve objekte iz environment
 
 # 3.a
 
-library()
+library() # izlista sve instalirane pakete ukljucene u radni prostor
 
 # 3.b
 
-install.packages("vcd") 
+install.packages("vcd") # instalira paket
 
 # 3.c 
 
-library("vcd", lib.loc="~/R/win-library/3.4") 
+library("vcd", lib.loc="~/R/win-library/3.4") # uvodi paket u radni prostor
 
 # 3.d
 
-data (package = "vcd")
+data (package = "vcd") # izlista sve skupove podataka u datom paketu
 
 
